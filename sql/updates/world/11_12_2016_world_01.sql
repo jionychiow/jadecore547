13e1e1b09c6fcb2301166c1aaf5f833569111d0f
cooler-SAI <cooler19860212@yahoo.com>
2016-12-12 22:49:14 +0300

    DB/Quest: Morning Breeze Village fix

--- sql/updates/world/11_12_2016_world_01.sql
+++ sql/updates/world/11_12_2016_world_01.sql
@@ -1,1 +1,2 @@
-UPDATE `quest_template` SET `Method` = 2, `SpecialFlags` = 0 WHERE `Id` = 29678;
+UPDATE `quest_template` SET `Method` = 2, `SpecialFlags` = 0 WHERE `Id` = 29678;  
+UPDATE `quest_template` SET `Method` = 2, `SpecialFlags` = 0 WHERE `Id` = 29776; 
